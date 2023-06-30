d4b799d2bff426d63b9d9b6c4676de9c2e6dc066
mechakin <barrientosryan83@gmail.com>
2023-06-30 01:00:10 -0400

    finish project

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,17 @@
+import { h } from "preact";
+import { useState } from "preact/hooks";
+
+export default function Greeting(props: { messages: string[] }) {
+  const { messages } = props;
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    </div>
+  );
+}
